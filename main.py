87a47d1d8d1ca5e4efc62664d3ad7180a304d6b8
crisarau <araujo.j.cristian@gmail.com>
2024-02-10 10:17:38 -0500

    final report done

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-    print(get_book_word_count(text))
-    print(get_letter_count(text))
+    word_count = get_book_word_count(text)
+    char_count_dict = get_char_count_dict(text)
+    letter_count_dict = get_letter_count_list(char_count_dict)
+    print_report(book_path, word_count,letter_count_dict)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 def get_book_word_count(text):
     return len(text.split())
-def get_letter_count(text):
+def get_char_count_dict(text):
     answer = {}
     for char in text:
         lower_char = char.lower()
@@ -19,4 +20,21 @@ def get_letter_count(text):
         else:
             answer[lower_char] = 1
     return answer
+def sort_on(dict):
+    return dict["num"]
+def get_letter_count_list(char_count_dict):
+    answer = []
+    for char, count in char_count_dict.items():
+        if char.isalpha():
+            answer.append(
+                {"letter": char, "num": count}
+            )
+    answer.sort(reverse=True, key=sort_on)
+    return answer
+def print_report(path, word_count, alpha_letter_count):
+    print(f"--- Being report of {path} ---")
+    print(f"{word_count} words found in the document\n")
+    for letter in alpha_letter_count:
+        print(f"The '{letter["letter"]}' character was found {letter["num"]} times")
+    print("--- End report ---")
 main()
