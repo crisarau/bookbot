768caa3c7440c4f460ba57b438b3d812265ae7ef
crisarau <araujo.j.cristian@gmail.com>
2024-02-10 09:55:47 -0500

    character counter

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ def main():
     text = get_book_text(book_path)
     print(text)
     print(get_book_word_count(text))
+    print(get_letter_count(text))
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 def get_book_word_count(text):
     return len(text.split())
+def get_letter_count(text):
+    answer = {}
+    for char in text:
+        lower_char = char.lower()
+        if lower_char in answer:
+            answer[lower_char] += 1
+        else:
+            answer[lower_char] = 1
+    return answer
 main()
